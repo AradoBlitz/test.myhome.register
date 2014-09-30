012d6fcf660c099c3845169cded45afe4597382d
AradoBlitz <schlachtnavaho@gmail.com>
2014-10-01 00:42:20 +0300

    Extracted connection to database statements from MonthPayment class to test class.

--- integration/test/myhome/register/RegisterDBIT.java
+++ integration/test/myhome/register/RegisterDBIT.java
@@ -2,6 +2,11 @@ package test.myhome.register;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.junit.Test;
 
 public class RegisterDBIT {
@@ -9,6 +14,19 @@ public class RegisterDBIT {
 	@Test 
 	public void getDataFromStorage() throws Exception{
 		MonthPayment monthPayment = new MonthPayment();
+		Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/seconddb;create=true");
+		try{
+			Statement stmt = conn.createStatement();
+			try{
+				
+			}finally{
+				
+				stmt.close();
+				
+			}
+		}finally{
+			conn.close();
+		}
 		monthPayment.readPayment();
 		assertEquals("Rent should be provided.", 3500, monthPayment.rent);
 	}
