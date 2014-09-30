9368b13bb2ae03d6b81ad85dfcf4ec1c59cca89c
AradoBlitz <schlachtnavaho@gmail.com>
2014-10-01 01:04:13 +0300

    Revert

--- src/test/myhome/register/MonthPayment.java
+++ src/test/myhome/register/MonthPayment.java
@@ -16,14 +16,20 @@ public class MonthPayment {
 		try{
 			Statement stmt = conn.createStatement();
 			try{
-				
+				stmt.execute("create table rent (rent integer)");
+				String strRent = "" + rent;
+		
+				stmt.execute("insert into rent (rent) values("  + strRent + ")");
 				ResultSet result = stmt.executeQuery("select * from rent");
 				result.next();
 				rent = result.getInt(1);
 			
 			}finally{
-				
-				stmt.close();
+				try{
+					stmt.execute("drop table rent");
+				}finally{
+					stmt.close();
+				}
 			}
 		}finally{
 			conn.close();
